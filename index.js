2ae32e67368128aae15b31f6f562ba9bcfd24d57
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-06 18:52:52 -0500

    added json parsing middleware to index, fleshed out auth route, as well as added validation using JOI

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config')
 
+//Import Routes
+const authRoute = require('./routes/auth');
+
+
 // Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },  () => {
     console.log('connected to db!')
 })
 
-//Import Routes
-const authRoute = require('./routes/auth');
+//Middleware
+app.use(express.json())
 
 // Route Middlewares
 app.use('/api/user', authRoute)
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const { registerValidation, loginValidation } = require('../validation')
 
-router.post('/register', (req, res) => {
-    res.send('Register')
+router.post('/register', async (req, res) => {
+
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    try{
+        const savedUser = await user.save()
+        res.send(savedUser)
+    } catch(err){
+        res.status(400).send(err)
+    }
 })
 
 module.exports = router;

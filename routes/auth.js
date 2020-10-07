03516e14a7483bfbdb4a70a5fa291a1ab2dc328f
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-06 19:12:52 -0500

    added validation on register route to check if user was already in db, also encrypted passwords and only returned the user id upon registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,30 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrpyt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
-
+    //Validation
+    const { error } = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message)
 
+    // Check if user is already in database
+    const emailExist = await User.findOne({email: req.body.email})
+    if(emailExist) return res.status(400).send('Email already exists');
+
+    //Hash password
+    const salt = await bcrpyt.genSalt(10);
+    const hashedPassword = await bcrpyt.hash(req.body.password, salt)
+
+    // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword
     })
     try{
         const savedUser = await user.save()
-        res.send(savedUser)
+        res.send({user: user._id})
     } catch(err){
         res.status(400).send(err)
     }

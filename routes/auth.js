c5c56ecfd629c6a6ddf4a6b1d9a2dec7209b9075
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-06 19:23:30 -0500

    implemented JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken')
 const bcrpyt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('../validation')
 
@@ -42,7 +43,9 @@ router.post('/login', async (req, res) => {
     const validPass = await bcrpyt.compare(req.body.password, user.password);
     if(!validPass) return res.status(400).send('invalid password')
 
-    res.send('logged in!')
+    // Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
+    res.header('auth-token', token).send(token)
 })
 
 module.exports = router;
